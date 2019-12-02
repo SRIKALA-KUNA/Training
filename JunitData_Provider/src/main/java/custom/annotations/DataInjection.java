64328f3d815c9ebd6fc64d4e21702367619fe702
Srikala Kuna <srikala_kuna@epam.com>
2019-12-02 18:07:50 +0530

    data provider

---
+++ JunitData_Provider/src/main/java/custom/annotations/DataInjection.java
@@ -0,0 +1,17 @@
+package custom.annotations;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target({METHOD, TYPE})
+public @ interface DataInjection {	  
+	public String dataProvider() default "";
+	//public Class<?> dataProviderClass() default Object.class;
+	
+}
